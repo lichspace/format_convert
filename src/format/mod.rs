fc37ccba47338ed747750255a2e5081180b578b9
tangentguo-win <tangentguo@tencent.com>
2024-10-23 17:13:38 +0800

    feat: gusess_format 失败采用后缀名

--- src/format/mod.rs
+++ src/format/mod.rs
@@ -1,5 +1,7 @@
 extern crate imagefmt;
 use std::{fs::File, io::Read};
+use image::ImageFormat;
+
 use crate::utils::{save, set_background};
 
 pub fn convert(src: &str, dist: &str, transparent: bool) {
@@ -9,9 +11,13 @@ pub fn convert(src: &str, dist: &str, transparent: bool) {
     file.read_to_end(&mut buffer).expect("无法读取文件");
     let bbox = buffer.into_boxed_slice();
     let buf = bbox.into_iter().as_slice();
-    let format = image::guess_format(buf).unwrap();
-    let image = image::load_from_memory_with_format(buf, format).unwrap();
+    let mut  format_df = ImageFormat::from_path(src).unwrap();
+    let gusess_format = image::guess_format(buf);
+    if gusess_format.is_ok() {
+        format_df = gusess_format.unwrap();
+    }
+    let image = image::load_from_memory_with_format(buf, format_df).unwrap();
     let mut image = image.to_rgba8();
-    image = set_background(image.clone(), transparent);
+    image = set_background(image, transparent);
     save(dist, image);
 }
